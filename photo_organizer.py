fc04bcc74d63e84704127eb92734d4c235e61963
Pedro Sa <pedro.sousa.sa@gmail.com>
2023-03-14 17:23:45 +0000

    refactor

--- photo_organizer.py
+++ photo_organizer.py
@@ -1,38 +1,61 @@
-import os
 import time
+import os
 import glob
 from shutil import copy2
 
 
-def photo_date(path: str) -> tuple:
-    timestamp = time.gmtime(os.path.getmtime(path))
-    return timestamp.tm_year, timestamp.tm_mon, timestamp.tm_mday
+class Photo:
+    def __init__(self, source_path):
+        self.path = source_path
+
+    @property
+    def photo_year(self):
+        return time.gmtime(os.path.getmtime(self.path)).tm_year
+
+    @property
+    def photo_month(self):
+        return time.gmtime(os.path.getmtime(self.path)).tm_mon
+
+    @property
+    def photo_day(self):
+        return time.gmtime(os.path.getmtime(self.path)).tm_mday
 
 
-def run(input_path, output_path, file_ext=['ORF', 'JPG']):
-    """
-    Copy files to destination folder, creating year, month, and day directories if not exist
-    :param input_path: where the files are located
-    :param output_path: base dir to copy the fiels to
-    :param file_ext: list of files ext - default to ['ORF', 'JPG']
-    :return: nothing
-    """
-    # get input photos paths
-    photos_paths = [path for path in glob.glob(os.path.join(input_path, '*')) if path[-3:] in file_ext]
+class PhotoCopier:
+    FILE_TYPES = ('ORF', 'JPG')
 
-    for photo_path in photos_paths:
+    def __init__(self, source_dir, destination_dir):
+        self.source_dir = source_dir
+        self.destination_dir = destination_dir
 
-        photo_year, photo_month, photo_day = photo_date(input_path)
+    def destination_hierarchy_folder(self, photo: Photo):
+        return os.path.join(self.destination_dir,
+                            str(photo.photo_year),
+                            f'{photo.photo_month:02}',
+                            f'{photo.photo_day:02}')
 
-        destination_path = os.path.join(output_path, str(photo_year), f'{photo_month:02}', f'{photo_day:02}')
+    def photos_paths(self):
+        return [path for path in glob.glob(os.path.join(self.source_dir, '*')) if path[-3:] in ('ORF', 'JPG')]
 
-        if not os.path.isdir(destination_path):
-            os.makedirs(destination_path)
-        else:
-            destination_photo_path = os.path.join(destination_path, os.path.basename(photo_path))
-            if os.path.isdir(destination_photo_path):
-                continue
+    @staticmethod
+    def create_dir_if_not_exist(path):
+        if not os.path.isdir(path):
+            os.makedirs(path)
 
-        copy2(photo_path, destination_path)
+    def run(self):
+        for photo_source_path in self.photos_paths():
+            photo = Photo(photo_source_path)
+            destination_path = self.destination_hierarchy_folder(photo)
+
+            self.create_dir_if_not_exist(destination_path)
+
+            copy2(photo_source_path, destination_path)
 
 
+if __name__ == '__main__':
+    source_dir = r'E:\DCIM\100OLYMP'
+    destination_dir = ''
+
+    photo_copier = PhotoCopier(source_dir, destination_dir)
+
+    photo_copier.run()
